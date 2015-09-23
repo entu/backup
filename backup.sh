205f2c8e5bed86d7a10a36483727c089bfb1d9fd
Argo Roots <argo@roots.ee>
2015-09-23 18:37:46 +0300

    Removed daily_ prefix

--- backup.sh
+++ backup.sh
@@ -8,17 +8,17 @@ data_dir=/usr/src/entu-backup
 
 cd ${data_dir}
 
-MYSQL_PWD=$MYSQL_PASSWORD mysql -h$MYSQL_HOST -u$MYSQL_USER -Bse "SELECT DISTINCT TABLE_SCHEMA FROM information_schema.TABLES WHERE TABLE_SCHEMA <> 'information_schema' ORDER BY TABLE_SCHEMA;" > daily_databases.txt
+MYSQL_PWD=$MYSQL_PASSWORD mysql -h$MYSQL_HOST -u$MYSQL_USER -Bse "SELECT DISTINCT TABLE_SCHEMA FROM information_schema.TABLES WHERE TABLE_SCHEMA <> 'information_schema' ORDER BY TABLE_SCHEMA;" > databases.txt
 
 while read database
 do
 
-    MYSQL_PWD=$MYSQL_PASSWORD mysql -h$MYSQL_HOST -u$MYSQL_USER -Bse "SELECT DISTINCT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '${database}' AND TABLE_TYPE = 'BASE TABLE' ORDER BY TABLE_NAME;" > daily_${database}.txt
-    MYSQL_PWD=$MYSQL_PASSWORD mysqldump ${database} -h$MYSQL_HOST -u$MYSQL_USER --single-transaction `cat daily_${database}.txt` | gzip -9 > daily_${database}.sql.gz
+    MYSQL_PWD=$MYSQL_PASSWORD mysql -h$MYSQL_HOST -u$MYSQL_USER -Bse "SELECT DISTINCT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '${database}' AND TABLE_TYPE = 'BASE TABLE' ORDER BY TABLE_NAME;" > ${database}.txt
+    MYSQL_PWD=$MYSQL_PASSWORD mysqldump ${database} -h$MYSQL_HOST -u$MYSQL_USER --single-transaction `cat ${database}.txt` | gzip -9 > ${database}.sql.gz
 
     s3file=${database}/${database}_`date +"%Y-%m-%d_%H-%M-%S"`.sql.gz
 
-    s3cmd put --quiet --config=${data_dir}/s3cfg --access_key=$S3_KEY --secret_key=$S3_SECRET --acl-private --no-progress --server-side-encryption --multipart-chunk-size-mb=640 daily_${database}.sql.gz $S3_BUCKET/daily/${s3file} 2>&1
+    s3cmd put --quiet --config=${data_dir}/s3cfg --access_key=$S3_KEY --secret_key=$S3_SECRET --acl-private --no-progress --server-side-encryption --multipart-chunk-size-mb=640 ${database}.sql.gz $S3_BUCKET/daily/${s3file} 2>&1
 
     if [ `date +"%u"` -eq 1 ]
     then
@@ -30,12 +30,12 @@ do
         s3cmd sync --quiet --config=${data_dir}/s3cfg --access_key=$S3_KEY --secret_key=$S3_SECRET --acl-private --no-progress --server-side-encryption --multipart-chunk-size-mb=640 $S3_BUCKET/daily/${s3file} $S3_BUCKET/monthly/${database}/ 2>&1
     fi
 
-    rm daily_${database}.txt
-    rm daily_${database}.sql.gz
+    rm ${database}.txt
+    rm ${database}.sql.gz
 
-done < daily_databases.txt
+done < databases.txt
 
-rm daily_databases.txt
+rm databases.txt
 
 echo "`date +"%Y-%m-%d %H:%M:%S"` BACKUP DONE"
 echo ""
